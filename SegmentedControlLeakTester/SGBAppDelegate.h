4f9f7aaaa1c2460e8e07b38f779380f84f9bff3b
Simon Booth <simon.booth@credit360.com>
2013-04-23 09:30:45 +0100

    Initial Commit

---
+++ SegmentedControlLeakTester/SGBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGBAppDelegate.h
+//  SegmentedControlLeakTester
+//
+//  Created by Simon Booth on 23/04/2013.
+//  Copyright (c) 2013 Simon Booth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SegmentedControlLeakTester/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SegmentedControlLeakTester
+//
+//  Created by Simon Booth on 23/04/2013.
+//  Copyright (c) 2013 Simon Booth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGBAppDelegate class]));
+    }
+}
